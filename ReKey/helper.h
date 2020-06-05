53638452463beb59475d9d96dd2e0f623c6625ef
1LAE <vermakov@cs.petrsu.ru>
2020-06-05 20:58:01 +0300

    opencv dep

--- ReKey/helper.h
+++ ReKey/helper.h
@@ -4,8 +4,8 @@
 #include <QObject>
 #include <QString>
 #include <string>
-#include <opencv2/core.hpp>
-#include <opencv2/videoio.hpp>
+#include <core.hpp>
+#include <videoio.hpp>
 
 
 typedef struct Video{
