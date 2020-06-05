9e9448014751d4bea3b2a0ecda803b806bb50d35
1LAE <vermakov@cs.petrsu.ru>
2020-06-06 00:47:43 +0300

    NEVERGONNALETUOYDOWN

--- ReKey/main.cpp
+++ ReKey/main.cpp
@@ -1,27 +1,10 @@
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
-#include <QQmlContext>
-
 #include "helper.h"
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    int a;
 
-    QGuiApplication app(argc, argv);
+    load("aaaaa");
 
-    QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-
-    Helper helper;
-    engine.rootContext()->setContextProperty("helper", &helper);
-
-    engine.load(url);
-
-    return app.exec();
+    return 0;
 }
