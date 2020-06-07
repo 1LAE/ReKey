2f9c811bb52026a9c6c1e925be5a219653d8acb6
1LAE <vermakov@cs.petrsu.ru>
2020-06-07 20:40:06 +0300

    hhh

--- ReKey/helper.cpp
+++ ReKey/helper.cpp
@@ -11,80 +11,80 @@
 //#include "opencv2/videoio.hpp"
 #include "helper.h"
 
-#include </usr/local/include/opencv4/opencv2/imgproc.hpp>
-#include </usr/local/include/opencv4/opencv2/imgcodecs.hpp>
-#include </usr/local/include/opencv4/opencv2/core.hpp>
-#include </usr/local/include/opencv4/opencv2/videoio.hpp>
+//#include <opencv2/imgproc.hpp>
+//#include <opencv2/imgcodecs.hpp>
+//#include <opencv2/core.hpp>
+//#include <opencv2/videoio.hpp>
 
-using namespace cv;
+//using namespace cv;
 
-Video vid;
+//Video vid;
 
-bool load(QString path){
+//bool load(QString path){
 
-    vid.path = path;
+//    vid.path = path;
 
-    QFile file(path);
-    if(!file.open(QIODevice::ReadOnly)){
-        std::cerr << "No such file found\n";
-        return false;
-    }
-    file.close();
+//    QFile file(path);
+//    if(!file.open(QIODevice::ReadOnly)){
+//        std::cerr << "No such file found\n";
+//        return false;
+//    }
+//    file.close();
 
-    if(vid.capture.isOpened()){
-        vid.capture.release();
-    }
+//    if(vid.capture.isOpened()){
+//        vid.capture.release();
+//    }
 
-    if(!(bool)vid.capture.open(path.toStdString())){
-        std::cerr << "Cant open video file\n";
-        return false;
-    }
+//    if(!(bool)vid.capture.open(path.toStdString())){
+//        std::cerr << "Cant open video file\n";
+//        return false;
+//    }
 
-    vid.totalFrames = vid.capture.get(CAP_PROP_FRAME_COUNT);
-    vid.fps = vid.capture.get(CAP_PROP_FPS);
-    vid.height = vid.capture.get(CAP_PROP_FRAME_HEIGHT);
-    vid.width = vid.capture.get(CAP_PROP_FRAME_WIDTH);
+//    vid.totalFrames = vid.capture.get(CAP_PROP_FRAME_COUNT);
+//    vid.fps = vid.capture.get(CAP_PROP_FPS);
+//    vid.height = vid.capture.get(CAP_PROP_FRAME_HEIGHT);
+//    vid.width = vid.capture.get(CAP_PROP_FRAME_WIDTH);
 
-    Mat image;
-    image = imread((const std::string)"../ReKey/template.jpg", IMREAD_COLOR);
-    if(image.empty()){
-        return false;
-    }
+//    Mat image;
+//    image = imread((const std::string)"../ReKey/template.jpg", IMREAD_COLOR);
+//    if(image.empty()){
+//        return false;
+//    }
 
-    imwrite("../Rekey/image.jpg", image);
-    imwrite("../Rekey/image_alpha.jpg", image);
+//    imwrite("../Rekey/image.jpg", image);
+//    imwrite("../Rekey/image_alpha.jpg", image);
 
-    return true;
-}
+//    return true;
+//}
 
 
-void prerender(QString key, int hue, int sat, int val, int ai, int time){
+//void prerender(QString key, int hue, int sat, int val, int ai, int time){
 
-    if(time == 100){
-        time = 99;
-    }
+//    if(time == 100){
+//        time = 99;
+//    }
 
-    int current = round(vid.totalFrames * ((double)time / 100));
+//    int current = round(vid.totalFrames * ((double)time / 100));
 
-    Mat frame, prerender;
+//    Mat frame, prerender;
 
-    vid.capture.set(CAP_PROP_POS_FRAMES, current);
-    vid.capture.read(frame);
-    vid.capture.read(prerender);
+//    vid.capture.set(CAP_PROP_POS_FRAMES, current);
+//    vid.capture.read(frame);
+//    vid.capture.read(prerender);
 
-    process(&prerender, convert(toRGB(key)), hue, sat, val, ai);
+//    process(&prerender, convert(toRGB(key)), hue, sat, val, ai);
 
-    double scale;
-    if((double)vid.width / vid.height >= (double)250/140){
-        scale = (double)500 / vid.width;
-    } else {
-        scale = (double)280 / vid.height;
-    }
+//    double scale;
+//    if((double)vid.width / vid.height >= (double)250/140){
+//        scale = (double)500 / vid.width;
+//    } else {
+//        scale = (double)280 / vid.height;
+//    }
 
-    Size size;
-    Mat crop;
-    resize(frame, crop, size, scale, scale, INTER_LINEAR);
-    imwrite("../ReKey/image.jpg", crop);
-    resize(prerender, crop, size, scale, scale, INTER_LINEAR);
-    imwrite("../ReKey/image_alpha.jpg", crop);
-}
+//    Size size;
+//    Mat crop;
+//    resize(frame, crop, size, scale, scale, INTER_LINEAR);
+//    imwrite("../ReKey/image.jpg", crop);
+//    resize(prerender, crop, size, scale, scale, INTER_LINEAR);
+//    imwrite("../ReKey/image_alpha.jpg", crop);
+//}
